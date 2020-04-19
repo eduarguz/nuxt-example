1460956abee99801cc2c0fc4b7665b229abf1e7f
Eduardo Guzmán Hermandez <eduarguzher@gmail.com>
2020-04-19 16:01:24 -0500

    seo tags

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,19 @@
     <h1>Create Event</h1>
   </div>
 </template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Where you can create events'
+        }
+      ]
+    }
+  }
+}
+</script>
